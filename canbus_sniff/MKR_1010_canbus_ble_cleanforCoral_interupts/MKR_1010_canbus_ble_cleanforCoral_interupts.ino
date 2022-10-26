19b706745eb1c736d0f733928a02cfa28e0c56b5
test3teamapp <test3teamapp@gmail.com>
2022-10-27 01:53:29 +0300

    arduino can bus withe interrupts

--- canbus_sniff/MKR_1010_canbus_ble_cleanforCoral_interupts/MKR_1010_canbus_ble_cleanforCoral_interupts.ino
+++ canbus_sniff/MKR_1010_canbus_ble_cleanforCoral_interupts/MKR_1010_canbus_ble_cleanforCoral_interupts.ino
@@ -14,9 +14,8 @@
 
 #include <ArduinoBLE.h>
 #include <CAN.h>
-#include <Wire.h>
 #include <MKRIMU.h>
-//#include "QuickMedianLib.h"
+//#include <ArduinoLowPower.h>
 
 BLEService carService("0000dd30-76d9-48e9-aa47-d0538d18f701");  // creating the service
 
@@ -25,24 +24,24 @@ BLEUnsignedIntCharacteristic rpmReading("0000dd32-76d9-48e9-aa47-d0538d18f701", 
 BLEFloatCharacteristic gyroXReading("0000dd33-76d9-48e9-aa47-d0538d18f701", BLERead | BLENotify);  // creating the heading angle Value characteristic
 
 
-long currentMillis = 0;
-long previousMillis = 0;
+long canbusInterruptTimeInSeconds = 0;
+long lastTimeRPMReceivedInSeconds = 0;
 int canbus_speed = 0;
 int canbus_rpm = 0;
 int canbus_dataload[8];
+// CanBus shiled Pin used to trigger a wakeup (from documentation)
+const int canbusInterruptPin = 7;
 BLEDevice central;
-bool bleClientConnected = false;
-bool newCanbusData = false;
+bool newCanbusData = false; // new relevant data received (speed and RPM)
+bool canbusIsReceiving = false; // can bus shield is receiving from the can bus (all data)- triggered with interrupt
 float gyro_x_accel = 0.0;
 
 //timekeepeing
 #define seconds() (millis() / 1000)
-// dummy speed;
-int prevSpeed = 0;
+const unsigned long deepsleeptime = 60000; // Sleep for 4 hours before waking up to check the battery
 
 
 void setup() {
-  Wire.begin();          // i2c dfrobot accelerometer id =  0x018
   Serial.begin(9600);//115200);  // initialize serial communication
   // while (!Serial);       //starts the program if we open the serial monitor.
 
@@ -59,6 +58,15 @@ void setup() {
     while (1)
       ;
   }
+
+  // Set interupt function that is triggered when data is received by the can bus
+  CAN.onReceive(onCanBusReceive);
+  // set interupt pin to wake up the main loop from deep sleep when
+  // the can bus shield receives data
+  //pinMode(canbusInterruptPin, INPUT);
+  // Attach a wakeup interrupt on the CanBus shield interrupt pin
+  // trigger function Interrupt Service Routine (ISR)
+  //LowPower.attachInterruptWakeup(canbusInterruptPin, ISR, RISING);
 
 
   if (!IMU.begin()) {
@@ -82,9 +90,6 @@ void setup() {
 
   BLE.advertise();  // start advertising the service
   Serial.println("Bluetooth device active, waiting for connections...");
-  // previousMillis = millis();
-  bleClientConnected = false;
-  newCanbusData = false;
 
   //time
   Serial.println(seconds());
@@ -92,30 +97,81 @@ void setup() {
 
 void loop() {
 
-  // dummy speed
-  // -----
-  int speed = (seconds() % 10) * 10;
-  if (speed != prevSpeed) {
-    prevSpeed = speed;
-    Serial.print("s");
-    Serial.println(speed);
-    Serial.print("gx");
-    Serial.println(gyro_x_accel);
-    speedReading.writeValue(speed);
-    gyroXReading.writeValue(gyro_x_accel);
-    rpmReading.writeValue(speed * 10);
-    
-  }
-  //currentMillis = millis();
-  // ------
+  // ------ THIS LOOP WILL BE TRIGGERED BY AN INTERRUPT BY THE CAN BUS SHIELD ---//
+  //Serial.println("loop is running");
 
   if (!central) {
+    //Serial.println("connecting to central");
+    //BLE.advertise();  // start advertising the service
     central = BLE.central();  // wait for a BLE central
+  }else if (! central.connected()) {
+    central.connect();
   }
-  // do the main can-bus sniffing
-  // try to parse packet
 
-  int packetSize = CAN.parsePacket();
+  if (newCanbusData) {
+    sendBLEData();
+  }
+
+  if (central && central.connected()) {  // if a central is connected to the peripheral
+    //Serial.print("Connected to central: ");
+    // Serial.println(central.address());  // print the central's BT address
+    digitalWrite(LED_BUILTIN, HIGH);  // turn on the LED to indicate the connection
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);  // when the central disconnects, turn off the LED
+    //Serial.print("Disconnected from central: ");
+    // Serial.println(central.address());
+  }
+
+  if (seconds() - lastTimeRPMReceivedInSeconds > 10) {
+    // THE ENGINE HAS STOPPED for almost 3 seconds
+    newCanbusData = false;
+    canbus_speed = canbus_speed + 1;
+    canbus_rpm = canbus_speed + 1;
+    sendBLEData(); // sent the last values. especially RPM 0
+    // The last value for RPM could have been other than 0
+    // so the board receiving from ble RPM values,
+    // can not know if engine is on or off,
+    // untill it receives an RPM = 0, so that it stops object detection
+
+    delay(10000); // wait 10 seconds.
+    // just so that BLE does not switch off before sending values
+    // Set MKR 1010 to low power
+    //LowPower.sleep();
+    //------- FOR TESTING ---//
+    lastTimeRPMReceivedInSeconds = seconds();
+    // Triggers a 10000 ms sleep (the device will be woken up only by the registered wakeup sources and by internal RTC)
+    // The power consumption of the chip will drop consistently
+    //if (central && central.connected()){
+    //  central.disconnect();
+    //}
+    //Serial.println("going to sleep");
+    //LowPower.deepSleep(8 * 1000); // low power sleeping is ms * 4
+  }
+}
+
+// Interrupt Service Routine // triggered on RISING
+// this will wakeup the loop from deep sleep
+void ISR()
+{
+  canbusIsReceiving = true;
+  newCanbusData = false; // wait for the appropriate data (speed, rpm)
+  canbusInterruptTimeInSeconds = seconds(); // mark when we woke up
+}
+
+void sendBLEData() {
+  if (central && central.connected()) {
+    speedReading.writeValue(canbus_speed);
+    rpmReading.writeValue(canbus_rpm);
+    if (canbus_speed > 0) {
+      // read accelaration data
+      acceleration();
+      gyroXReading.writeValue(gyro_x_accel);
+    }
+  }
+}
+
+// the interupt function , triggered by the canbus shiled when data arrives
+void onCanBusReceive(int packetSize) {
 
   if (packetSize) {
 
@@ -143,23 +199,11 @@ void loop() {
       }
       if (canbus_dataload[1] != 12 && canbus_dataload[2] != 80) {  // when turning on/off the lights bytes 1 and 2 are switching momentarily
         canbus_speed = canbus_dataload[2];                         // word(canbus_dataload[3], canbus_dataload[2]);// / 100;
-        Serial.print("s");
-        Serial.print(canbus_speed);
-        Serial.println();
-        if (central && central.connected()) {
-          speedReading.writeValue(canbus_speed);
-        }
-        if (canbus_speed > 0) {
-          // read accelaration data
-          acceleration();
-
-          if (central && central.connected()) {
-            gyroXReading.writeValue(gyro_x_accel);
-          }
-        }
+        newCanbusData = true;
       }
 
     } else if (CAN.packetId() == 0x3B3) {
+
       //                            ID      bit-start   bit-count
       // engine-rev (low refresh)   0x3b3   0           16
       // RPM  e.g.  id      data length   data
@@ -170,45 +214,28 @@ void loop() {
         canbus_dataload[i] = CAN.read();
         i++;
       }
-
       canbus_rpm = canbus_dataload[0];  // word(canbus_dataload[1], canbus_dataload[0]);
-      Serial.print("r");
-      Serial.print(canbus_rpm);
-      Serial.println();
-      // newCanbusData = true;
-      if (central && central.connected()) {
-        rpmReading.writeValue(canbus_rpm);
-      }
+      newCanbusData = true;
+      // RPM messages come many times within a second
+      // when engine is turned off, they stop comming
+      // with engine but power on, other messages may arrive
+      // So we use the RPM messages as indication of car engine running
+      lastTimeRPMReceivedInSeconds = seconds();
     }
-  }
-
-  // test
-  //  read accelaration data
-  acceleration();
-
-  if (central && central.connected()) {  // if a central is connected to the peripheral
-    // Serial.print("Connected to central: ");
-    // Serial.println(central.address());  // print the central's BT address
-    digitalWrite(LED_BUILTIN, HIGH);  // turn on the LED to indicate the connection
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);  // when the central disconnects, turn off the LED
-    // Serial.print("Disconnected from central: ");
-    // Serial.println(central.address());
   }
 }
 
 /*!
- *  @brief Print the position result.
- */
+    @brief get the G angle of the car on the x axis
+*/
 void acceleration(void) {
-  delay(50);
-  
+  //delay(50);
+
   float x, y, z;
   if (IMU.gyroscopeAvailable()) {
     IMU.readGyroscope(x, y, z);
-    // the way we install it in the car, to measure the "car's x g movement" we 
+    // the way we install it in the car, to measure the "car's x g movement" we
     // need to track the devices' "y" axis
     gyro_x_accel = y;
-    //Serial.println(y);
   }
 }
--- python/coralmini_python_requirements.sh
+++ python/coralmini_python_requirements.sh
@@ -3,4 +3,5 @@ sudo apt-get dist-upgrade
 sudo apt-get install python3-pip
 python3 -m pip install --upgrade pip setuptools --user
 python3 -m pip install bleak
+sudo apt-get install -y python3-dev libasound2-dev
 python3 -m pip install simpleaudio
